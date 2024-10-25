88e2f63177cb84ed1409f0eaa80d14fc6d678cdd
ncamuss <sebacamus@me.com>
2024-10-24 21:06:11 -0300

    version 2

--- trabajos grupales/EG4.3/assets/js/index.js
+++
@@ -1,6 +0,0 @@
-const obtenerNombreEmpresa = (empresa, nombreEmpresa) =>
-    empresa 
-        ? empresa.mostrarImportaciones()
-        : alert(`La empresa ${nombreEmpresa} no existe`);
-
-export {obtenerNombreEmpresa}
